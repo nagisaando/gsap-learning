b8bd2e45e9c4a6c9a978a0c57d3fc6ba2bfb0079
Nagisa Ando <amnot.nobig.deal@gmail.com>
2024-10-23 11:44:57 -0500

    make two buttons

--- src/button.ts
+++ src/button.ts
@@ -4,37 +4,106 @@ export function animateButton() {
   for (let i = 1; i <= 3; i++) {
     document.querySelector(
       "#buttons"
-    )!.innerHTML += `<button id="button-${i}">Button ${i}</button>`;
+    )!.innerHTML += `<button id="button-${i}">Hello world ${i}</button>`;
   }
 
   animateFirstButton();
+  animateSecondButton();
 }
 
 function animateFirstButton() {
-  const button1 = document.querySelector("#button-1") as HTMLButtonElement;
-  button1?.appendChild(document.createElement("span"));
+  const button = document.querySelector("#button-1") as HTMLButtonElement;
+  button?.appendChild(document.createElement("span"));
 
-  const buttonSpan = button1!.querySelector("span");
-
-  const hoverAnimation = gsap.timeline({ paused: true }).to(buttonSpan, {
-    scale: 100,
-    duration: 0.1,
-    backgroundColor: "#eca400",
-    ease: "circ.out",
-  });
-
-  button1?.addEventListener("mouseenter", (e: MouseEvent) => {
+  const buttonSpan = button!.querySelector("span");
+  function getCursorPosition(e: MouseEvent) {
     const target = e.target as HTMLElement;
     const rect = target.getBoundingClientRect();
 
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
-    console.log("mouse enter!!");
-    hoverAnimation.play();
+
+    return { x, y };
+  }
+  button?.addEventListener("mouseenter", (e: MouseEvent) => {
+    const mousePosition = getCursorPosition(e);
+
+    gsap.to(buttonSpan, {
+      scale: 9,
+      duration: 0.2,
+      backgroundColor: "#eca400",
+      ease: "circ.out",
+      top: mousePosition.y + "px",
+      left: mousePosition.x + "px",
+      right: undefined,
+    });
   });
 
-  button1?.addEventListener("mouseleave", () => {
-    console.log("mouse enter!!");
-    hoverAnimation.reverse();
+  button?.addEventListener("mouseleave", (e: MouseEvent) => {
+    const target = e.target as HTMLElement;
+    const elementData = target.getBoundingClientRect();
+
+    const mousePosition = getCursorPosition(e);
+
+    let position: {
+      top?: string;
+      bottom?: string;
+      left?: string;
+      right?: string;
+    } = {};
+
+    if (mousePosition.y < 0) {
+      position.top = "-20px";
+    } else if (elementData.height - mousePosition.y < 0) {
+      position.bottom = "-20px";
+    }
+
+    if (mousePosition.x < 3) {
+      position.left = "-20px";
+    } else if (elementData.width - mousePosition.x < 3) {
+      position.right = "-20px";
+    }
+
+    gsap.timeline().to(buttonSpan, {
+      scale: 0,
+      duration: 0.2,
+      backgroundColor: "transparent",
+      ease: "circ.out",
+      ...position,
+    });
+  });
+
+  button?.addEventListener("mousemove", (e: MouseEvent) => {
+    // const target = e.target as HTMLElement
+    const mousePosition = getCursorPosition(e);
+    gsap.to(buttonSpan, {
+      top: mousePosition.y + "px",
+      left: mousePosition.x + "px",
+    });
+  });
+}
+
+function animateSecondButton() {
+  const button = document.querySelector("#button-2") as HTMLButtonElement;
+  button.innerHTML = `<span>button</span>`;
+
+  const buttonSpan = button!.querySelector("span");
+
+  button?.addEventListener("mouseenter", (e: MouseEvent) => {
+    gsap
+      .timeline()
+      .to(buttonSpan, {
+        yPercent: -150,
+        duration: 0.2,
+        ease: "power2.in",
+      })
+      .set(buttonSpan, {
+        yPercent: 150,
+      })
+      .to(buttonSpan, {
+        yPercent: 0,
+        duration: 0.2,
+        ease: "power2.in",
+      });
   });
 }
