78e8bee8dd5ec56f8710581c356c6ce7aceb9f77
Nagisa Ando <amnot.nobig.deal@gmail.com>
2024-10-25 11:43:03 -0500

    add physuc 2d animation

--- src/time-control.ts
+++ src/time-control.ts
@@ -1,12 +1,21 @@
 import gsap from "gsap";
+import Physics2DPlugin from "gsap-trial/Physics2DPlugin";
 
+gsap.registerPlugin(Physics2DPlugin);
 export function timeControl() {
   document.getElementById("time-control")!.innerHTML = `
     <button id="play-button">play</button>
     <button id="pause-button">pause</button>
     <button id="reverse-button">reverse</button>
     <button id="restart-button">restart</button>
+    <button id="test-button">test</button>
   `;
+  let star = document.querySelector(".star");
+  let container = document.querySelector(".star-container");
+  for (let i = 0; i < 10; i++) {
+    let cloneStar = star!.cloneNode();
+    container!.appendChild(cloneStar);
+  }
 
   var animation = gsap
     .timeline()
@@ -22,15 +31,52 @@ export function timeControl() {
       stagger: 0.05,
       ease: "back.out",
     })
-    .from(".flower", { duration: 2, rotate: 360, ease: "back.out", scale: 0 });
+    .add("test")
+    .from(".flower", { duration: 2, rotate: 360, ease: "back.out", scale: 0 })
+
+    .fromTo(
+      ".star",
+      {
+        duration: 0.2,
+        scale: 0,
+      },
+      {
+        duration: 0.2,
+        scale: "random(0.2, 0.6)",
+      },
+      "-=2"
+    )
+    .to(
+      ".star",
+      {
+        duration: 1.5,
+        physics2D: {
+          velocity: "random(500, 750)",
+          angle: "random(250, 290)",
+          gravity: 800,
+        },
+        stagger: 0.05,
+      },
+      "-=2"
+    )
+    .to(
+      ".star",
+      {
+        duration: 0.5,
+        scale: 0,
+      },
+      "-=0.7"
+    );
 
   const playButton = document.querySelector("#play-button");
   const restartButton = document.querySelector("#restart-button");
   const reverseButton = document.querySelector("#reverse-button");
   const pauseButton = document.querySelector("#pause-button");
+  const testButton = document.querySelector("#test-button");
 
   playButton?.addEventListener("click", () => animation.play());
   pauseButton?.addEventListener("click", () => animation.pause());
   reverseButton?.addEventListener("click", () => animation.reverse());
   restartButton?.addEventListener("click", () => animation.restart());
+  testButton?.addEventListener("click", () => animation.play("test"));
 }
